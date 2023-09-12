3eb0db7a5c2f6bb396866580ba274ca8800366d6
Ali <211407036@ogr.mku.edu.tr>
2023-09-12 18:37:05 +0300

    Figure With Startes

---
+++ figure with stars/figure with stars/Program.cs
@@ -0,0 +1,78 @@
+﻿
+// Klavyeden Girilen n Sayısı Kadar Yıldız
+
+/*
+
+int sayı, i, j;
+
+Console.Write("Bir Sayı Giriniz:");
+sayı=Convert.ToInt32(Console.ReadLine());   
+                    
+for(i = 0; i <= sayı; i++)
+{
+    for(j = 1; j <= i; j++)                                 // Örnek 1
+    {
+        Console.Write("*");
+    }
+    Console.WriteLine();
+} 
+        // sayı = 4
+        // *
+        // **
+        // ***
+        // ****
+
+*/
+
+/*
+
+int sayı, i, j;
+
+Console.Write("Bir Sayı Giriniz: ");
+sayı = Convert.ToInt32(Console.ReadLine());
+
+for (i = sayı; i > 0; i--)
+{                                               // Örnek 2
+    for (j = 1; j <= i; j++)
+    {
+        Console.Write("*");
+    }
+    Console.WriteLine();
+}
+    // sayı = 3
+    // ***
+    // **
+    // *
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
